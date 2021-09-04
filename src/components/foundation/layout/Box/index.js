5c0bba23753256f204ee260ddf594ca9f7fa6250
lucaspires-source <pires.lucas94@gmail.com>
2021-09-04 13:12:40 -0300

    created Component Box

---
+++ src/components/foundation/layout/Box/index.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import propToStyle from '../../../../theme/utils/propToStyle';
+
+const Box = styled.div`
+  ${propToStyle('flex')}
+  ${propToStyle('display')}
+  ${propToStyle('flexDirection')}
+  ${propToStyle('justifyContent')}
+  ${propToStyle('alignItems')}
+  ${propToStyle('flexWrap')}
+  ${propToStyle('backgroundImage')}
+  ${propToStyle('backgroundRepeat')}
+  ${propToStyle('backgroundPosition')}
+  ${propToStyle('min-width')}
+  ${propToStyle('min-height')}
+`;
+
+export default Box;
